c8dbdc15332678c8d6204865d38dbe2cc9ae2e5b
Francis Beeson <leobeeson@gmail.com>
2022-06-09 13:08:11 +0100

    Build dictionary, bow vectors/corpus, tf-idf model

--- 00_core_concepts.py
+++ 00_core_concepts.py
@@ -1,5 +1,8 @@
 from data.stopwords import stopwords_english
 
+from gensim import corpora
+from gensim import models
+
 from collections import defaultdict
 
 import re
@@ -38,3 +41,24 @@ processed_corpus = [
     for text in texts
     ]
 
+# BUILD DICTIONARY https://radimrehurek.com/gensim/corpora/dictionary.html
+dictionary = corpora.Dictionary(processed_corpus)
+print(dictionary)
+dictionary.token2id # dict[term: dictionary_id]
+dictionary.cfs # dict[dictionary_id: term_frequency] -> term frequency within corpus
+dictionary.dfs # dict[dictionary_id: doc_frequency] -> # docs in which term appears
+dictionary.num_docs # int -> # of docs processed
+dictionary.num_pos # int -> # of processed terms
+dictionary.num_nnz # int -> ∑ of # of unique terms per doc across corpus
+
+# BOW VECTORS
+new_doc = "python and java developer needed by UK startup"
+new_vec = dictionary.doc2bow(new_doc.lower().split())
+
+# BOW CORPUS
+bow_corpus = [dictionary.doc2bow(token) for token in processed_corpus]
+
+# TF-IDF MODEL
+tf_idf = models.TfidfModel(bow_corpus)
+words = "java python".lower().split()
+print(tf_idf[dictionary.doc2bow(words)])
