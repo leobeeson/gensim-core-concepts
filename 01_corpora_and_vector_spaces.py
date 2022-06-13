88c415c97dc460802e09a48b0c3c1172533d1723
Francis Beeson <leobeeson@gmail.com>
2022-06-13 12:03:07 +0100

    Fix errors in streaming documents to create BOW

--- 01_corpora_and_vector_spaces.py
+++ 01_corpora_and_vector_spaces.py
@@ -17,7 +17,7 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 # READ IN CORPUS
 with open("data/sample_corpus_software_engineering.json") as json_file:
     sample_corpus_json = json.load(json_file)
-sample_corpus = sample_corpus_json["sample_corpus"][0:10]
+sample_corpus = sample_corpus_json["corpus"][0:10]
 
 # REMOVE PUNCTUATION
 pattern = re.compile("\.|,|;|\?|!|\(|\)|\[|\]")
@@ -48,15 +48,28 @@ processed_corpus = [
 # BUILD DICTIONARY
 dictionary = corpora.Dictionary(processed_corpus)
 dictionary.save("/tmp/stackexchange_first_ten_tags.dict")
-print(dictionary)
+# print(dictionary)
 
 # BOW VECTORS
 new_doc = "python and java developer needed by UK startup"
 new_vec = dictionary.doc2bow(new_doc.lower().split())
-print(new_vec)
+# print(new_vec)
 
 # BOW CORPUS
 bow_corpus = [dictionary.doc2bow(token) for token in processed_corpus]
 corpora.MmCorpus.serialize("/tmp/stackexchange_first_ten_tags.mm", bow_corpus)
-print(bow_corpus)
+# print(bow_corpus)
 
+from smart_open import open  # for transparently opening remote files
+
+class MyCorpus:
+    def __iter__(self):
+        for line in open("data/sample_corpus_software_engineering_small.txt"):
+            # assume there's one document per line, tokens separated by whitespace
+            yield dictionary.doc2bow(line.lower().split())
+
+corpus_memory_friendly = MyCorpus()  # doesn't load the corpus into memory!
+print(corpus_memory_friendly)
+
+for vector in corpus_memory_friendly:  # load one vector into memory at a time
+    print(vector)
--- 02_corpus_streaming.py
+++ 02_corpus_streaming.py
@@ -7,7 +7,7 @@ import re
 
 
 stoplist = stopwords_english["stopwords"]
-pattern = re.compile("\n|\. |,|;|\?|!|\(|\)|\[|\]")
+pattern = re.compile("\. |,|;|\?|!|\(|\)|\[|\]")
 
 dictionary = corpora.Dictionary()
 
@@ -26,5 +26,3 @@ for vector in corpus:
 
 
 
-sample_text = "javascript\njavascript\njavascript\njavascript\njavascript\njavascript\njavascript\njavascript\njavascript\njavascript\njavascript\njavascript\njavascript\njavascript\njavascript\njavascript\njavascript\njavascript\njavascript\njavascript\n\njs\necmascript\n.js\njavascript-execution\nvanilla-js\nvanillajs\njavascript-library\njavascript-runtime\nvanilla-javascript\njavascript-module\nclassic-javascript\njavascript-alert\njavascript-dom\njavascript-disabled\nFor questions regarding programming in ECMAScript (JavaScript/JS) and its various dialects/implementations (excluding ActionScript). Note JavaScript is NOT the same as Java! Please include all relevant tags on your question; e.g., [node.js], [jquery], [json], [reactjs], [angular], [ember.js], [vue.js], [typescript], [svelte], etc."
-new_text = re.sub("\n", " ", sample_text)
