1e3241917d7c33e8f42518e5098966c0cbb9a90e
Francis Beeson <leobeeson@gmail.com>
2022-06-15 23:38:54 +0100

    Create corpus using bespoke MyDictionary class

--- 03_topics_and_transformations.py
+++ 03_topics_and_transformations.py
@@ -1,4 +1,6 @@
 from classes.my_dictionary import MyDictionary
+from data.stopwords import stopwords_english
+from smart_open import open
 
 import logging
 
@@ -7,6 +9,15 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 
 CORPUS_FILEPATH = "data/sample_corpus_software_engineering_small.txt"
+STOPWORDS = stopwords_english["stopwords"]
 
 
-dictionary = MyDictionary(CORPUS_FILEPATH)
+my_dict = MyDictionary(CORPUS_FILEPATH)
+texts = [
+    [
+      word for word in document.lower().split() if word not in STOPWORDS
+    ]
+for document in open(CORPUS_FILEPATH)
+]
+
+corpus = [my_dict.dictionary.doc2bow(text) for text in texts]
